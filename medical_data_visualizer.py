2a72d70dac523d7fd80d4de174fbbc1690e43188
Vinicius Testa Passos <viniciustestapassos@gmail.com>
2024-09-22 17:30:31 -0300

    feat: adiciona visualização de dados médicos

--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -4,55 +4,88 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # 1
-df = None
+df = pd.read_csv('medical_examination.csv')
 
 # 2
-df['overweight'] = None
+df['overweight'] = np.where(
+    (df['weight'] / ((df['height'] / 100) ** 2)) > 25, 
+    1, 0)
 
 # 3
+df['cholesterol'] = np.where(df['cholesterol'] == 1, 0, 1)
+df['gluc'] = np.where(df['gluc'] == 1, 0, 1)
 
-
-# 4
 def draw_cat_plot():
     # 5
-    df_cat = None
-
-
-    # 6
-    df_cat = None
+    df_cat = pd.melt(
+        df, 
+        id_vars=['cardio'], 
+        value_vars=[
+            'cholesterol', 
+            'gluc', 
+            'smoke', 
+            'alco', 
+            'active', 
+            'overweight'
+        ]
+    )
     
-
+    # 6
+    
     # 7
-
-
-
+    fig = sns.catplot(
+        x='variable',
+        hue='value', 
+        col='cardio', 
+        data=df_cat,  
+        kind='count',  
+        height=5,  
+        aspect=1.2,  
+        order=['active', 'alco', 'cholesterol', 'gluc', 'overweight', 'smoke']
+    )
+    fig.set_ylabels('total')
+    
     # 8
-    fig = None
-
+    fig.fig.subplots_adjust(top=0.9)
+    fig.fig.suptitle('Contagem de Valores Categóricos por Variável e Cardio')
 
     # 9
     fig.savefig('catplot.png')
-    return fig
+    
+    return fig.fig
 
 
 # 10
 def draw_heat_map():
     # 11
-    df_heat = None
+    df_heat = df[
+        (df['height'] >= df['height'].quantile(0.025)) &
+        (df['height'] <= df['height'].quantile(0.975)) &
+        (df['weight'] >= df['weight'].quantile(0.025)) &
+        (df['weight'] <= df['weight'].quantile(0.975))
+    ]
 
     # 12
-    corr = None
+    corr = df_heat.corr()
 
     # 13
-    mask = None
-
-
+    mask = np.triu(corr)
 
     # 14
-    fig, ax = None
+    fig, ax = plt.subplots(figsize=(12, 12))
 
     # 15
-
+    sns.heatmap(
+        corr, 
+        annot=True, 
+        fmt='.1f', 
+        linewidths=1, 
+        mask=mask, 
+        square=True, 
+        cbar_kws={
+            'shrink': 0.5
+        }
+    )
 
 
     # 16
